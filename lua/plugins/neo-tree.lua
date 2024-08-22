1875bbe3a88f3b8ce42a1281b089d4b9b415ccea
Michael Kimani <kimanimyke@gmail.com>
2024-08-22 20:25:34 +0300

    add hidden files to neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,24 @@ return{
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        -- add extension names you want to explicitly exclude
+                        -- '.git',
+                        -- '.DS_Store',
+                        -- 'thumbs.db',
+                    },
+                    never_show = {},
+                },
+            },
+        })
         vim.keymap.set('n', '<C-n>', ':Neotree toggle reveal filesystem left<CR>', {})
-        --vim.keymap.set('n', '<C-b>', ':Neotree reveal close', {})
-    end
+
+    end,
 }
